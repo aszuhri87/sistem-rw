dbae9b3c30a9dd780747b01ade11ed5c8bf48af1
Achmad Saefuddin Zuhri <achmad.s.zuhri182@gmail.com>
2022-12-12 13:55:15 +0700

    add seeder admin & penjimpit

--- database/seeders/AdminSeed.php
+++ database/seeders/AdminSeed.php
@@ -13,6 +13,22 @@ class AdminSeed extends Seeder
      */
     public function run()
     {
-        //
+        \App\Models\Admin::create([
+            'name' => 'Admin',
+            'email' => 'admin@bibitunggul.com',
+            'rt' => null,
+            'rw' => null,
+            'password' => Hash::make('bu1@varx.id'),
+            'level' => 'admin',
+        ]);
+
+        \App\Models\Admin::create([
+            'name' => 'Penjimpit',
+            'email' => 'penjimpit@bibitunggul.com',
+            'rt' => null,
+            'rw' => null,
+            'password' => Hash::make('bu2@varx.id'),
+            'level' => 'penjimpit',
+        ]);
     }
 }
